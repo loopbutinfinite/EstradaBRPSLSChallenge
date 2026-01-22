aa7c93ddba86dbfe35985c6c586442f653364152
loopbutinfite <bestrada8800@gmail.com>
2026-01-21 18:11:43 -0800

    added functionality to the frontend so that the endpoint received from the API is now displayed on the webpage.

--- Scripts/app.js
+++ Scripts/app.js
@@ -1,4 +1,4 @@
-const API_Base = "rpslsgamebe-ashvazctccbhg5c5.westus3-01.azurewebsites.net";
+const API_Base = "https://rpslsgamebe-ashvazctccbhg5c5.westus3-01.azurewebsites.net";
 
 const backToGamePageButton = document.getElementById("backToGamePageButton");
 const gameRulesPageButton = document.getElementById("gameRulesPageButton");
@@ -12,6 +12,7 @@ const spockImg = document.getElementById("spockImg");
 const paperImg = document.getElementById("paperImg");
 const rockImg = document.getElementById("rockImg");
 const scissorsImg = document.getElementById("scissorsImg");
+const headerText = document.getElementById("headerText");
 
 
 const fetchAPIUrl = async (userChoice) => {
@@ -33,24 +34,35 @@ const fetchAPIUrl = async (userChoice) => {
 //     scissorsImg.style.display = "none";
 //     scissorsButton.style.display = "none";
 // };
-paperButton.addEventListener("click", () => {
+
+paperButton.addEventListener("click", async () => {
     // removeIconsandButtons();
     const paper = paperButton.dataset.message;
-    fetchAPIUrl(paper);
+    const matchResult = await fetchAPIUrl(paper);
+    headerText.style.fontSize = "46px";
+    headerText.textContent = matchResult;
 });
-rockButton.addEventListener("click", () => {
+rockButton.addEventListener("click", async () => {
     const rock = rockButton.dataset.message;
-    fetchAPIUrl(rock);
+    const matchResult = await fetchAPIUrl(rock);
+    headerText.style.fontSize = "46px";
+    headerText.textContent = matchResult;
 });
-lizardButton.addEventListener("click", () => {
+lizardButton.addEventListener("click", async () => {
     const lizard = lizardButton.dataset.message;
-    fetchAPIUrl(lizard);
+    const matchResult = await fetchAPIUrl(lizard);
+    headerText.style.fontSize = "46px";
+    headerText.textContent = matchResult;
 });
-scissorsButton.addEventListener("click", () => {
+scissorsButton.addEventListener("click", async () => {
     const scissors = scissorsButton.dataset.message;
-    fetchAPIUrl(scissors);
+    const matchResult = await fetchAPIUrl(scissors);
+    headerText.style.fontSize = "46px";
+    headerText.textContent = matchResult;
 });
-spockButton.addEventListener("click", () => {
+spockButton.addEventListener("click", async () => {
     const spock = spockButton.dataset.message;
-    fetchAPIUrl(spock);
+    const matchResult = await fetchAPIUrl(spock);
+    headerText.style.fontSize = "46px";
+    headerText.textContent = matchResult;
 });
