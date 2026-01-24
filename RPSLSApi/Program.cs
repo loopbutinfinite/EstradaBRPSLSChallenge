2d11407af39b08e35f56820800715cbf524c3493
loopbutinfite <bestrada8800@gmail.com>
2026-01-24 02:15:59 -0800

    fixed the duplicate code in the CORS policy

--- RPSLSApi/Program.cs
+++ RPSLSApi/Program.cs
@@ -14,7 +14,7 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("CorsPolicy", policy =>
     {
-        policy.AllowAnyOrigin().AllowAnyHeader().AllowAnyOrigin();
+        policy.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod();
     });
 });
 
