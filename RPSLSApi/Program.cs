bc77c68b897df1a5d883bb5eb8341952bb20c603
loopbutinfite <bestrada8800@gmail.com>
2026-01-24 02:29:51 -0800

    altered the CORS policy

--- RPSLSApi/Program.cs
+++ RPSLSApi/Program.cs
@@ -12,10 +12,10 @@ builder.Services.AddScoped<RPSLSServices>();
 
 builder.Services.AddCors(options =>
 {
-    options.AddPolicy("CorsPolicy", policy =>
-    {
-        policy.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod();
-    });
+   options.AddPolicy("CorsRPSLSPolicy", policy =>
+   {
+       policy.WithOrigins("https://agreeable-smoke-07b729c1e.1.azurestaticapps.net", "http://agreeable-smoke-07b729c1e.1.azurestaticapps.net").AllowAnyHeader().AllowAnyMethod();
+   }); 
 });
 
 var app = builder.Build();
@@ -29,7 +29,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-app.UseCors("CorsPolicy");
+app.UseCors("CorsRPSLSPolicy");
 
 app.UseAuthorization();
 
